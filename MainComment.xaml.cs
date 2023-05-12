5d6cb9e166b5b6649aa5d6995fa459d38be0fdeb
Threem4y <Andreyka0193@mail.ru>
2023-05-12 23:47:41 +0300

    first commit

---
+++ MainComment.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Forum
+{
+    public partial class MainComment : Window
+    {
+        public static List<string> users = new List<string>();
+        private Socket socket;
+        public MainComment()
+        {
+            InitializeComponent();
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(/*"26.33.75.123"*/MainWindow.Ipi, 8888);
+            RecieveMessage();
+        }
+        private async Task RecieveMessage()
+        {
+            while (true)
+            {
+                byte[] bytes = new byte[1024];
+                await socket.ReceiveAsync(bytes, SocketFlags.None);
+                string message = Encoding.UTF8.GetString(bytes);
+                MessageListBox.Items.Add(message);
+            }
+        }
+        private async Task sendnudes(string msg)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(msg);
+            await socket.SendAsync(bytes, SocketFlags.None);
+
+        }
+
+        private void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageTextBox.Text == "/disconnect")
+            {
+                MainWindow qwe = new MainWindow();
+                qwe.Show();
+                Close();
+            }
+            sendnudes(MessageTextBox.Text);
+
+        }
+
+        private void MessageTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            MainWindow qwe = new MainWindow();
+            qwe.Show();
+            Close();
+        }
+
+        private void MessageListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Forum
+{
+    public partial class MainWindow : Window
+    {
+        public static string Ipi;
+         public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+            private void Join_Click(object sender, RoutedEventArgs e)
+        {
+                if (Ipi != null)
+                {
+                    Ipi = IpAdress.Text;
+                    MainComment server = new MainComment();
+                    server.Show();
+                    Close();
+                }
+
+
+                else
+                {
+                    MessageBox.Show("Введите IP форума");
+                }
+            }
+
+        private void IpAdress_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
